0dc8d224b6629db1734ab2e502ef534e3d21493f
Artem <artemzheldak1995@gmail.com>
2020-02-28 17:46:34 +0300

    Update EmptyStringException.cs

--- ExceptionHandlingModule/EmptyStringException.cs
+++ ExceptionHandlingModule/EmptyStringException.cs
@@ -6,9 +6,12 @@ namespace ExceptionHandlingModule
 {
     public class EmptyStringException : Exception
     {
-        public EmptyStringException()
-        {
-            Console.WriteLine("String is empty");
-        }
+        //public EmptyStringException()
+        //{
+          //  Console.WriteLine("String is empty");
+        //}
+        
+        public EmptyStringException(string message) : base(message)
+        { }
     }
 }
